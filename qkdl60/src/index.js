c47d63eee4c961bd2cbee2c99ef38de1d6433806
nocheol <qkdl60@gmail.com>
2024-07-28 18:31:31 +0900

    feat : autofocus 기능

--- qkdl60/src/index.js
+++ qkdl60/src/index.js
@@ -1,1 +1,11 @@
 // TODO: 이 곳에 정답 코드를 작성해주세요.
+const $signInButton = document.querySelector('#submit')
+$signInButton.addEventListener('click', (event) => {
+    event.preventDefault()
+})
+autofocus()
+
+function autofocus() {
+    const $idInput = document.querySelector('#id')
+    $idInput.focus()
+}
