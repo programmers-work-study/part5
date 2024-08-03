b49e8c0c1683262f739604abb81961064699bb15
pongdang <pongpong0121@kakao.com>
2024-08-03 23:22:46 +0900

    feat: 유효하지 않은 값일 경우, 각 경우에 맞는 에러 메시지를 보여주어야 합니다.

--- im-binary/src/utils/validation.js
+++ im-binary/src/utils/validation.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGE } from '../constants.js'
+
 export function validateId(id) {
     // 5~20자. 영문 소문자, 숫자. 특수기호(_),(-)만 사용 가능
     const idRegex = /^[a-z0-9_-]{5,20}$/
@@ -15,14 +17,30 @@ export function checkPassword(password, passwordConfirm) {
     return password === passwordConfirm
 }
 
+function setErrorMessage($input, message) {
+    const $errorElement = document.querySelector(`#${$input.id}-msg`)
+    $errorElement.textContent = message
+}
+
+function errorMessage({ type }) {
+    return ERROR_MESSAGE[type]
+}
+
 export function checkValidation($input, validationFunc) {
     const value = $input.value
 
+    if (value.trim() === '') {
+        $input.classList.add('invalid')
+        setErrorMessage($input, errorMessage({ type: 'required' }))
+        return false
+    }
     if (validationFunc(value)) {
         $input.classList.remove('invalid')
+        setErrorMessage($input, '')
         return true
     } else {
         $input.classList.add('invalid')
+        setErrorMessage($input, errorMessage({ type: $input.id }))
         return false
     }
 }
